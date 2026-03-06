bef2f2870ab991ba341adfb1cf3af6ab43865e50
Rifqi <mistermathindonesia@gmail.com>
2026-03-07 05:13:38 +0700

    part2

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const container = document.querySelector(".container");
-const square = document.createElement("div");
-container.appendChild(square)
 
-console.log(container);
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement("div");
+  square.style.width = "60px";
+  square.style.height = "60px";
+  square.style.backgroundColor = "black";
+  container.appendChild(square);
+
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "red";
+  });
+  // square.addEventListener("mouseout", () => {
+  //   square.style.backgroundColor = "black";
+  // });
+}
